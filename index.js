fb452b436f5ba28ffca02525b7cea4c8e67bb23e
Manamana <allentrobson@gmail.com>
2022-12-13 21:31:29 -0700

    completed add new dep/role/employee

--- index.js
+++ index.js
@@ -54,13 +54,13 @@ mainMenu = () => {
                     showEmployees();
                     break;
                 case "Add A Department":
-
+                    addNewDepartment();
                     break;
                 case "Add A Role":
-
+                    addNewRole();
                     break;
                 case "Add An Employee":
-
+                    addNewEmployee();
                     break;
                 case "Update An Employee Role":
 
@@ -73,6 +73,147 @@ mainMenu = () => {
         })
 
 }
+
+addNewDepartment = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'Input',
+                message: 'What is the name of your new department?',
+                name: 'departmentName',
+            }
+        ])
+        .then((data) => {
+            db.query("INSERT INTO department (department_name) VALUES (?);", data.departmentName, (err, results) => {
+                console.log("Successfully added new department");
+
+            })
+            mainMenu();
+        })
+}
+
+addNewRole = async () => {
+
+    var departments = [];
+
+    db.query(`SELECT * FROM department`, (err, results) => {
+
+
+        results.forEach(department => {
+            departments.push(department.department_name);
+
+        });
+    });
+
+    inquirer
+        .prompt([
+            {
+                type: 'Input',
+                message: 'What is the title of your new role?',
+                name: 'roleTitle',
+            },
+            {
+                type: 'Input',
+                message: 'What is the new roles salary?',
+                name: 'roleSalary',
+            },
+            {
+                type: 'list',
+                message: 'What is the department id that the new role is a part of?',
+                name: 'roleDepartmentId',
+                choices: departments,
+
+            }
+        ])
+        .then((data) => {
+
+            var depId;
+            for (let i = 0; i < departments.length; i++) {
+                if(departments[i]===data.roleDepartmentId){
+                    depId = i + 1;
+                }
+            }
+
+            db.query("INSERT INTO role (title, salary, department_id) VALUES (?,?,?);", [data.roleTitle, data.roleSalary, depId], (err, results) => {
+                console.log("Succesfully added new role");
+
+            })
+            mainMenu();
+        })
+}
+
+addNewEmployee = async () => {
+
+    var roles = [];
+    var employees = [];
+
+    db.query(`SELECT * FROM role`, (err, results) => {
+
+
+        results.forEach(role => {
+            roles.push(role.title);
+
+        });
+    });
+    db.query(`SELECT * FROM employee`, (err, results) => {
+
+
+        results.forEach(employee => {
+            employees.push(`${employee.first_name} ${employee.last_name}`);
+
+        });
+    });
+
+    inquirer
+        .prompt([
+            {
+                type: 'Input',
+                message: 'What is the employees first name?',
+                name: 'firstName',
+            },
+            {
+                type: 'Input',
+                message: 'What is the employees last name?',
+                name: 'lastName',
+            },
+            {
+                type: 'list',
+                message: 'Who is the employees manager?',
+                name: 'manager',
+                choices: employees,
+
+            },
+            {
+                type: 'list',
+                message: 'What is the employees title?',
+                name: 'title',
+                choices: roles,
+
+            }
+        ])
+        .then((data) => {
+
+            var managerId;
+            var roleId;
+            for (let i = 0; i < employees.length; i++) {
+                if(employees[i]===data.manager){
+                    managerId = i + 1;
+                }
+            }
+            for (let i = 0; i < roles.length; i++) {
+                if(roles[i]===data.title){
+                    roleId = i + 1;
+                }
+            }
+
+            db.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?);", [data.firstName, data.lastName, roleId, managerId], (err, results) => {
+                console.log("Succesfully added new employee");
+
+            })
+            mainMenu();
+        })
+}
+
 
 showDepartments = () => {
     db.query(`SELECT * FROM department`, (err, results) => {
